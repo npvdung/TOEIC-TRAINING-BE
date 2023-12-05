b74a8b8e85945165e723593b7419a76444759fc1
NPVDung <npvdrealmadrid@gmail.com>
2023-12-05 18:50:30 +0700

    fix connect db

--- reading/reading.model.js
+++ reading/reading.model.js
@@ -1,15 +1,15 @@
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
 
 module.exports = model;
 
 function model(sequelize) {
-    const attributes = {
-        paragraph: {type: DataTypes.STRING, allowNull: false},
-        title: {type: DataTypes.STRING, allowNull: false},
-        translate: {type: DataTypes.STRING, allowNull: true},
-        categoryId: {type: DataTypes.NUMBER, allowNull: true},
-        level: {type: DataTypes.NUMBER, allowNull: true}
-    };
+  const attributes = {
+    paragraph: { type: DataTypes.STRING, allowNull: false },
+    title: { type: DataTypes.STRING, allowNull: true },
+    translate: { type: DataTypes.STRING, allowNull: true },
+    categoryId: { type: DataTypes.NUMBER, allowNull: false },
+    level: { type: DataTypes.NUMBER, allowNull: true },
+  };
 
-    return sequelize.define("reading", attributes);
+  return sequelize.define("reading", attributes);
 }
